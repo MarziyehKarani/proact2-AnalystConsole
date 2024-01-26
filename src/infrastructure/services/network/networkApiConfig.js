edbff39df79cb5355a398ecad5d3174fbe20de69
Marziyeh <marziyeh.karani@engitel.com>
2024-01-26 09:48:55 +0100

    Initial commit

---
+++ src/infrastructure/services/network/networkApiConfig.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// function getDevEvironment() {
+//     return "https://devproactservices.azurewebsites.net/api";
+// }
+
+function getDevEvironment() {
+    return "https://localhost:44378/api";
+} 
+
+function getProdEnvironment() {
+    return "https://prodproactservices.azurewebsites.net/api";
+}
+
+export function setupApiConfiguration(languageTag) {
+    var environmentBaseUrl = getProdEnvironment();
+    axios.defaults.baseURL = `${environmentBaseUrl}/${languageTag}/`;
+}
+
+export function setApiAuthToken(authToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
+}
